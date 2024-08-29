29fc4330b062fd2f6e44498d0499c183f243ce97
Mihreteab Aride <misganawmihreteab@gmail.com>
2024-08-30 01:00:22 +0300

    Add TotalBalanceBox component

---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,27 @@
+import AnimatedCounter from "./AnimatedCounter";
+
+const TotalBalanceBox = ({
+  accounts = [],
+  totalBanks,
+  totalCurrentBalance,
+}: TotlaBalanceBoxProps) => {
+  return (
+    <section className="total-balance">
+      <div className="total-balance-chart">{/* Dou */}</div>
+
+      <div className="flex flex-col gap-6">
+        <h2 className="header-2">Bank accounts: {totalBanks}</h2>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <p className="total-balance-label">Total Current Balance</p>
+
+        <p className="total-balance-amount flex-center gap-2">
+          <AnimatedCounter amount={totalCurrentBalance} />
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default TotalBalanceBox;
