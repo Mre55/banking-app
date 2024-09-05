ad7065ececf2322cb000708089d791e47c5d4888
Mihreteab Aride <misganawmihreteab@gmail.com>
2024-09-05 12:10:55 +0300

    add mobile nav bar style

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -58,23 +58,22 @@ const MobileNav = ({ user }: MobileNavProps) => {
                       <Link
                         href={item.route}
                         key={item.label}
-                        className={cn("sidebar-link", {
+                        className={cn("mobilenav-sheet_close w-full", {
                           "bg-bank-gradient": isActive,
                         })}
                       >
-                        <div className="relative size-6">
-                          <Image
-                            src={item.imgURL}
-                            alt={item.label}
-                            fill
-                            className={cn({
-                              "brightness-[3] invert-0": isActive,
-                            })}
-                          />
-                        </div>
+                        <Image
+                          src={item.imgURL}
+                          alt={item.label}
+                          width={20}
+                          height={20}
+                          className={cn({
+                            "brightness-[3] invert-0": isActive,
+                          })}
+                        />
                         <p
-                          className={cn("sidebar-label", {
-                            "!text-white": isActive,
+                          className={cn("text-16 font-semibold text-black-2", {
+                            "text-white": isActive,
                           })}
                         >
                           {item.label}
@@ -83,8 +82,10 @@ const MobileNav = ({ user }: MobileNavProps) => {
                     </SheetClose>
                   );
                 })}
+                USER
               </nav>
             </SheetClose>
+            FOOTER
           </div>
         </SheetContent>
       </Sheet>
