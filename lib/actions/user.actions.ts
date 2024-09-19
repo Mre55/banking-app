4910c7769fc63f14c770112bb38f1f0408206b15
Mihreteab Aride <misganawmihreteab@gmail.com>
2024-09-19 03:26:30 -0700

    Add user actions file

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,16 @@
+"use server";
+
+export const signIn = async () => {
+  try {
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
+
+export const signUp = async (userData: SignUpParams) => {
+  try {
+    // Create user account
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
